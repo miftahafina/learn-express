fa9eeebfe521091b4eb7393ec8d3f5d51d3ac2ec
Miftah Afina <miftah.afina@gmail.com>
2018-10-07 05:37:51 +0700

    Route with params

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ var app = express();
 
 var things = require('./things.js');
 
+app.get('/:id/:name', (req, res) => {
+    res.send('id: ' + req.params.id + ' and name: ' + req.params.name);
+});
+
 app.all('/test', (req, res) => {
     res.send('Hello with all!');
 });
 
-app.use('/things', things);
 
 app.listen(3000);
