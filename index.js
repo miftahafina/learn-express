41f0c2056c2d60399bc3515f4a53f9e34e786dce
Miftah Afina <miftah.afina@gmail.com>
2018-10-07 05:42:08 +0700

    Default route (404)

--- index.js
+++ index.js
@@ -11,5 +11,10 @@ app.all('/test', (req, res) => {
     res.send('Hello with all!');
 });
 
+// Other routes (404)
+app.get('*', function(req, res){
+    res.send('404 not found, bruh!');
+});
 
+app.use('/things', things);
 app.listen(3000);
