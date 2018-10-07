41c265d4dc1c46f91286f31e4084370731cb4b3b
Miftah Afina <miftah.afina@gmail.com>
2018-10-07 08:47:57 +0700

    Pug with conditional and templating

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get('/first/:name', (req, res) => {
     });
 });
 
+app.get('/template', (req, res) => {
+    res.render('content');
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/things', (req, res, next) => {
