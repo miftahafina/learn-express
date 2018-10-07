0952a0a8f10e5b171c94c2303e5044fc902fd208
Miftah Afina <miftah.afina@gmail.com>
2018-10-07 09:36:44 +0700

    Receiving form data

--- index.js
+++ index.js
@@ -1,49 +1,29 @@
-var express = require('express');
-var app = express();
+var express    = require('express');
 var bodyParser = require('body-parser');
+var multer     = require('multer');
+var upload     = multer();
+var app        = express();
 
-var things = require('./things.js');
+app.get('/', (req, res) => {
+    res.render('form');
+});
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.get('/first/:name', (req, res) => {
-    res.render('first_view', {
-        name: req.params.name,
-        twitter: '@MiftahAfina'
-    });
-});
+// for parsing application/json
+app.use(bodyParser.json());
 
-app.get('/template', (req, res) => {
-    res.render('content');
-});
+// for parsing application/xwww-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/pictures', express.static('public'));
+// for parsing multipart/form-data
+app.use(upload.array());
+app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/things', (req, res, next) => {
-    console.log('A new request for things received at ' + Date.now());
-    next();
-});
-
-// app.use('/things', (req, res, next) => {
-//     console.log('A request for things received at ' + Date.now());
-// });
-
-app.get('/things/:id([0-9]{5})/:name', (req, res) => {
-    res.send('id: ' + req.params.id + ' and name: ' + req.params.name);
-});
-
-app.all('/test', (req, res) => {
-    res.send('Hello with all!');
-});
-
-app.use('/things', things);
-
-// Other routes (404)
-app.get('*', function(req, res){
-    res.send('404 not found, bruh!');
+app.post('/', function(req, res){
+    console.log(req.body);
+    res.send("Received your request!");
 });
 
 app.listen(3000);
