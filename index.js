7f17e0b061cc82287f85b049a54a38e4ae964a80
Miftah Afina <miftah.afina@gmail.com>
2018-10-07 14:06:33 +0700

    Make model

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 var express    = require('express');
 var bodyParser = require('body-parser');
 var multer     = require('multer');
+var mongoose   = require('mongoose');
 var upload     = multer();
 var app        = express();
 
-app.get('/', (req, res) => {
-    res.render('form');
-});
+// Connect
+mongoose.connect('mongodb://localhost/my_db',  {useNewUrlParser: true});
 
+// view engine
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 // for parsing application/json
 app.use(bodyParser.json());
-
 // for parsing application/xwww-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
 // for parsing multipart/form-data
 app.use(upload.array());
+// static dir
 app.use(express.static('public'));
 
-app.post('/', function(req, res){
-    console.log(req.body);
-    res.send("Received your request!");
+// Model
+var personSchema = mongoose.Schema({
+    name       : String,
+    age        : Number,
+    nationality: String
 });
+var Person = mongoose.model("Person", personSchema);
+
+app.get('/person', (req, res) => {
+    res.render('person');
+});
+
+app.post('/person', (req, res) => {
+    res.send(req.body);
+})
 
 app.listen(3000);
